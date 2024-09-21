cb26a7cd6d559ec83561f5f1bcab53012b82fb9e
AYYAN-TECH <wsobia24@yahoo.com>
2024-09-21 17:52:00 +0100

    Add files via upload

--- zombe.js
+++ zombe.js
@@ -9,4 +9,16 @@ AFRAME.registerComponent('zombie-feast', {
             posX += 50;
             this.zombieSpawn(id, position);
         }
-    }})
+    }, 
+    zombieSpawn:function(id,position){
+        var environEl = document.querySelector('a-scene');
+        var zombEl = document.createElement('a-entity');
+        zombEl.setAttribute('id', id);
+        zombEl.setAttribute("position",position)
+        zombEl.setAttribute("gltf-model","#B")
+        zombEl.setAttribute("scale","10 10 10")
+        zombEl.setAttribute('animation-mixer', { clip: 'Walk' });
+        zombEl.setAttribute('animation', {property: 'position', to: '-10 0 1000', easing: 'linear', loop: 'true', dur: '1500000'});
+        environEl.appendChild(zombEl);
+    }
+})
